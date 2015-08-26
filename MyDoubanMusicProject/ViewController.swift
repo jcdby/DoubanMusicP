a844ac83e511baa1f7e75d17634d5dfb07cb9b1b
Ethan <jcdby@hotmail.com>
2015-08-26 20:31:38 +0900

    Initiation

---
+++ MyDoubanMusicProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyDoubanMusicProject
+//
+//  Created by 李金橙 on 15/8/26.
+//  Copyright (c) 2015年 李金橙. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
